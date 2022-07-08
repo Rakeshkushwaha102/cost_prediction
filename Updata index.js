27322a0c9e2b09795eada9d4e17f827d06e10c9a
Rakeshkushwaha102 <108785848+Rakeshkushwaha102@users.noreply.github.com>
2022-07-08 15:11:08 +0530

    Create Updata index.js

---
+++ Updata index.js
@@ -0,0 +1,34 @@
+import http from 'http';
+//var port = process.env.PORT 11 8000;
+const CORS(
+   'Access-Control-Allow-Origin': '*'
+   'Access-Control-Allow-Methods: 'GET,POST,DELETE, OPTIONS',
+   'Access-Control-Allow-Headers: 'Content-Type, Access-Control-Allow-Headers, x-test'
+ };
+
+const server = http.createServer(async(req, res)=>{
+      if(req.url = '/result4/'){
+
+          res.writeHead (200, {
+              'Content-Type': 'application/json',
+              ...CORS,
+          })
+          let data = '';
+          await req.on('data', function(chunk){
+           data += chunk;
+           }).on('end', () => {
+         })
+         res.write(JSON.stringify({
+           "message": "itmo308555",
+           "x-result":"baovn",
+           "x-body":"abc"
+           }
+           ))
+    }
+     res.end()
+
+ });
+ server.listen(4321,()=>(
+      console.log('Server is running')});
+
+
